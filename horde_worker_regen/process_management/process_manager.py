19218145135c67ea1db8b5c02a04fdc59115a1b4
Gabriel <gabrielfranz31@gmail.com>
2026-03-19 22:54:30 +0100

    Revert "fix: use injected horde_model_reference_manager and fix log message"
    This reverts commit 35d0487a13b2e9501a4f935142727c7c6e767213.
    
    Revert

--- horde_worker_regen/process_management/process_manager.py
+++ horde_worker_regen/process_management/process_manager.py
@@ -1592,11 +1592,12 @@ class HordeWorkerProcessManager:
 
         while self.stable_diffusion_reference is None:
             try:
-                all_refs = self.horde_model_reference_manager.get_all_model_references(overwrite_existing=False)
+                horde_model_reference_manager = ModelReferenceManager()
+                all_refs = horde_model_reference_manager.get_all_model_references(overwrite_existing=False)
                 _sd_ref = all_refs.get(MODEL_REFERENCE_CATEGORY.image_generation)
 
                 if not isinstance(_sd_ref, dict):
-                    logger.error("Image generation model references not found. Retrying in 5 seconds...")
+                    logger.error("Stable diffusion model references not found. Retrying in 5 seconds...")
                     time.sleep(5)
                     continue
 
